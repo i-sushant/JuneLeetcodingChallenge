91cefe0d36ff91dd6f33940529528f0a2841c53e
Sushant Gupta <imsushant25@gmail.com>
2020-06-06 19:57:11 +0530

    Day 6 added

---
+++ Day6.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Day6 {
+
+    public static int[][] reconstructQueue(int[][] people) {
+        List<int[]> res = new ArrayList<>();
+        Arrays.sort(people, (a, b) -> {
+            return a[0] == b[0] ? a[1] - b[1] : b[0] - a[0];
+        });
+        for (int[] x : people) {
+            res.add(x[1], x);
+        }
+        return res.toArray(new int[people.length][2]);
+    }
+}
