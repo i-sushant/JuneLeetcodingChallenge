374039e70b6696a034339bd5efa185cfef9895c0
Sushant Gupta <imsushant25@gmail.com>
2020-06-06 20:18:25 +0530

    update day 6

--- Day6.java
+++ Day6.java
@@ -2,14 +2,20 @@ import java.util.*;
 
 public class Day6 {
 
+    public static void main(String[] args) {
+        int[][] people = { { 7, 0 }, { 4, 4 }, { 7, 1 }, { 5, 0 }, { 6, 1 }, { 5, 2 } };
+        int[][] ans = reconstructQueue(people);
+        for (int[] x : ans) {
+            System.out.println(x[0] + ", " + x[1]);
+        }
+    }
+
     public static int[][] reconstructQueue(int[][] people) {
         List<int[]> res = new ArrayList<>();
-        Arrays.sort(people, (a, b) -> {
-            return a[0] == b[0] ? a[1] - b[1] : b[0] - a[0];
-        });
+        Arrays.sort(people, Comparator.comparing((int[] e) -> e[0]).reversed().thenComparing((int[] e) -> e[1]));
         for (int[] x : people) {
             res.add(x[1], x);
         }
-        return res.toArray(new int[people.length][2]);
+        return res.toArray(people);
     }
 }
